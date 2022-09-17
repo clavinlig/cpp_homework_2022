d233e5106795554ca608e1098b9dd5db5d217945
clavinlig <timoshenko.aa@phystech.edu>
2022-09-17 20:20:23 +0300

    Add task 3

---
+++ homework1/task3/task3/task3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+int main()
+{
+	int x, y;
+	std::cout << "print x:" << " ";
+	std::cin >> x;
+	std::cout << "print y:" << " ";
+	std::cin >> y;
+
+	x = x + y;
+	y = x - y;
+	x = x - y;
+
+	std::cout << "x = " << x << '\n' << "y = " << y;
+
+	return 0;
+}
