462addb469fdcff1998afe2189db13a8c2bf5291
clavinlig <timoshenko.aa@phystech.edu>
2022-10-01 12:30:33 +0300

    add alien and binsearch

---
+++ homework2/alien/alien/alien.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+
+int main()
+{
+    std::vector < std::string > result;
+    std::string symbol = "";
+    std::string text;
+
+    std::cout << "Enter the text :" << '\n';
+    getline(std::cin, text);
+
+    uint64_t pos = 0;
+    while (pos < text.size())
+    {
+        if (std::isspace(text[pos]) || (std::ispunct(text[pos]) && text[pos] != '-'))
+        {
+            if (symbol.size())
+                result.push_back(symbol);
+
+            if (std::ispunct(text[pos]))
+            {
+                symbol = text[pos];
+                result.push_back(symbol);
+            }
+            symbol = "";
+        }
+        else
+        {
+            symbol += text[pos];
+        }
+        ++pos;
+    }
+
+    std::cout << "Result:\n";
+    for (std::string k : result)
+    {
+        std::cout << k << ' ';
+    }
+
+    return 0;
+}
---
+++ homework2/binsearch/binsearch/binsearch.cpp
@@ -0,0 +1,47 @@
+﻿#include<vector>
+#include <iostream>
+//*
+int main()
+{
+	int len;
+	int x;
+
+	std::cout << "Please, input length of your sorted array: ";
+	std::cin >> len;
+
+	std::vector <int> arr(len);
+
+	std::cout << "Please, input your array: ";
+
+	for (size_t i = 0; i < len; ++i)
+	{
+		std::cin >> arr[i];
+	}
+	std::cout << "Please, input a required value: ";
+	std::cin >> x;
+
+	int left = 0, right = len - 1;
+
+
+	while (left < right)
+	{
+		int middle = (left + right) / 2;
+		if (arr[middle] < x)
+		{
+			left = middle + 1;
+		}
+		else
+		{
+			right = middle - 1;
+		}
+	}
+	if (arr[left] == x)
+	{
+		std::cout << "there is such element";
+		return 0;
+	}
+	std::cout << "there is no such element";
+
+	return 0;
+}
+
