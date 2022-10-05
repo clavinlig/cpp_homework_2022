80923e82bcaafe70e7eaa92d76f0ec4a6d6c6ba5
clavinlig <timoshenko.aa@phystech.edu>
2022-10-05 21:18:09 +0300

    Insertion_sort and questions are added

---
+++ homework2/insertion_sort/insertion_sort/insertion_sort.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+	int len;
+	std::vector <int> arr;
+
+	std::cout << "Please, input length of your array: ";
+	std::cin >> len;
+	std::cout << "Please, input your array: ";
+	for (size_t i = 0; i < len; ++i)
+	{
+		int number;
+		std::cin >> number;
+		arr.push_back(number);
+	}
+
+	for (size_t i = 1; i < len; i++)
+	{
+		int j = i;
+		while (j > 0 && arr[j] < arr[j - 1])
+		{
+			std::swap(arr[j], arr[j - 1]);
+			j -= 1;
+		}
+	}
+
+	std::cout << "Here is your sorted array: ";
+	for (size_t i = 0; i < len; i++)
+	{
+		std::cout << arr[i] << " ";
+	}
+	return 0;
+}
